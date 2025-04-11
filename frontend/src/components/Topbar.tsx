c5e757c68ecdf6bd58953f808b355c7562e0c375
dakshgoswami <dakshgoswami95@gmail.com>
2025-04-11 08:30:11 +0530

    beatbuzz logo routing

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -62,7 +62,7 @@ const Topbar = () => {
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
       <div className="flex flex-col items-start justify-center">
-        <div className="flex items-center max-sm:text-sm text-xl">
+        <div className="flex items-center max-sm:text-sm text-xl" onClick={() => navigate("/")}>
           <span className="font-bold text-white">B</span>eat
           <span className="font-bold text-white">B</span>uzz
         </div>
